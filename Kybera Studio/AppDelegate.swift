9f7310c18cf160b440bd6df523e48c66b8878805
Roman Ustinov <roman@progalgo.name>
2025-05-21 22:26:01 +0300

    Initial Commit

---
+++ Kybera Studio/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Kybera Studio
+//
+//  Created by Roman Ustinov on 21.05.2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Kybera StudioTests/Kybera_StudioTests.swift
@@ -0,0 +1,17 @@
+//
+//  Kybera_StudioTests.swift
+//  Kybera StudioTests
+//
+//  Created by Roman Ustinov on 21.05.2025.
+//
+
+import Testing
+@testable import Kybera_Studio
+
+struct Kybera_StudioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
